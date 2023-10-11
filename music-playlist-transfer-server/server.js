d560d1d5b66ad7d3195b853fa5e75f606baed067
EldenArgentum <mutahir_2002@icloud.com>
2023-10-11 13:28:12 -0500

    ignore

--- music-playlist-transfer-server/server.js
+++ music-playlist-transfer-server/server.js
@@ -43,7 +43,7 @@ app.get('/spotify/token', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
