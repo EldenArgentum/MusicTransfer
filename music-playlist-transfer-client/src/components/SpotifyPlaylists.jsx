1acdc937aff27e088ec1027fe51310ea97bb8dc1
EldenArgentum <mutahir_2002@icloud.com>
2024-01-10 14:47:51 -0600

    stumped

--- music-playlist-transfer-client/src/components/SpotifyPlaylists.jsx
+++ music-playlist-transfer-client/src/components/SpotifyPlaylists.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid'
+import { Button } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 
 const SpotifyPlaylists = ({ playlists, loading }) => {
+
+  const [selectedRows, setSelectedRows] = useState()
 
   const columns = [
     { field: 'title', headerName: 'Title', width: 300 },
@@ -17,6 +21,26 @@ const SpotifyPlaylists = ({ playlists, loading }) => {
     }
   })
 
+  const handleRowSelection = (e) => {
+    const playlistIds = e.map(playlist => {
+      return {
+        'id': e
+      }
+    }
+    )
+    setSelectedRows(playlistIds)
+  };
+
+  const playlistIdQuery = useQuery({
+    queryKey: ["selectedRows"],
+    queryFn: () => {return selectedRows},
+    enabled: !!selectedRows
+})
+
+  const handleButtonClick = () => {
+    console.log(selectedRows)
+  }
+
   return (
     <div>
     <DataGrid
@@ -31,7 +55,11 @@ const SpotifyPlaylists = ({ playlists, loading }) => {
       }}
       pageSizeOptions={[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
       checkboxSelection
-    />
+      onRowSelectionModelChange={(e) => handleRowSelection(e)}
+      >
+    </DataGrid>
+
+    <Button onClick={() => handleButtonClick}>Please CLick me</Button>
     </div>
   );
 };
