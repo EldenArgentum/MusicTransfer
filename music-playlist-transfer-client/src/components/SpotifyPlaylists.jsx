d95bb1d591948a90660c0d96d8bca777e8d5dc0f
EldenArgentum <mutahir_2002@icloud.com>
2024-06-27 09:50:05 -0500

    commit before Jotai?

--- music-playlist-transfer-client/src/components/SpotifyPlaylists.jsx
+++ music-playlist-transfer-client/src/components/SpotifyPlaylists.jsx
@@ -1,53 +1,51 @@
-import React, { useState } from 'react'
-import { DataGrid } from '@mui/x-data-grid'
+import React, { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 
 const SpotifyPlaylists = ({ playlists, loading }) => {
-
-  const [selectedRows, setSelectedRows] = useState()
-  const [buttonClicked, setButtonClicked] = useState('')
-
-  const playlistIdQuery = useQuery({
-    queryKey: ["selectedRows"],
-    queryFn: () => {return selectedRows},
-    enabled: !!buttonClicked
-  })
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
   const columns = [
     { field: 'title', headerName: 'Title', width: 300 },
     { field: 'tracks', headerName: '# of Tracks', width: 130 },
-    { field: 'id', headerName: 'ID', width: 80}
+    { field: 'id', headerName: 'ID', width: 80 }
   ];
-  
+
   const rows = playlists.map(playlist => {
     return {
       'title': playlist.name,
       'tracks': playlist.tracks.total,
       'id': playlist.id
-    }
-  })
+    };
+  });
 
+  const handleButtonClick = () => {
+    setButtonClicked(true);
+  };
 
   return (
     <div>
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      loading={loading}
-      initialState={{
-        columns: {
-          columnVisibilityModel: {id: false},
-        },
-        pagination: {paginationModel: { page: 0, pageSize: 10 }},
-      }}
-      pageSizeOptions={[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
-      checkboxSelection
-      onRowSelectionModelChange={(e) => setSelectedRows(e.rows)}
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        loading={loading}
+        initialState={{
+          columns: {
+            columnVisibilityModel: { id: false },
+          },
+          pagination: { paginationModel: { page: 0, pageSize: 10 } },
+        }}
+        pageSizeOptions={[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+        checkboxSelection
+        onRowSelectionModelChange={(ids) => {
+          const selectedRowsData =
+        }}
       >
-    </DataGrid>
+      </DataGrid>
 
-    <Button onClick={() => handleButtonClick}>Please CLick me</Button>
+      <Button onClick={() => console.log(selectedRows)}>Please Click me</Button>
     </div>
   );
 };
