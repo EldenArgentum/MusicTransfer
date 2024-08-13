51924b738fd958e149259bed8b854cf801662c4c
EldenArgentum <mutahir_2002@icloud.com>
2024-08-13 17:36:56 -0500

    no more ...

--- music-playlist-transfer-client/src/components/SpotifyPlaylists.jsx
+++ music-playlist-transfer-client/src/components/SpotifyPlaylists.jsx
@@ -41,7 +41,7 @@ const SpotifyPlaylists = ({ playlists, loading, selectedRowsAtom }) => {
 				pageSizeOptions={[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
 				checkboxSelection
 				onRowSelectionModelChange={(ids) => {
-					setSelectedRows([...ids])
+					setSelectedRows(ids)
 				}}
 			></DataGrid>
 
